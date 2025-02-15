d9f5ca5450a18ed239541293e455525485587401
Ella Barnes <ellabarnes@utexas.edu>
2025-02-14 21:17:57 -0600

    added markdown/comments moved event_cat to cleaning

--- 03-render-one-file-example.R
+++ 03-render-one-file-example.R
@@ -1,7 +1,7 @@
 library(quarto)
 
 quarto_render(
-  input = "02-report-template-example.qmd",
-  output_file = "album_test.html",
-  execute_params = list(album = "1989,1989 (Deluxe)")
+  input = "02-report-template-example.qmd", # What file we're wanting to use.
+  output_file = "album_test.html", # Name of rendered file.
+  execute_params = list(album = "1989,1989 (Deluxe)") # All of the parameters you are hoping to filter for.
 )
